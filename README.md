ebf4ee2a879ee869a56130be4f6638f4e2d58a09
TurboBasic <off@boun.cr>
2017-06-17 03:00:46 +0300

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Powershell-MVC-Pipeline-Cmdlet
 Template of Windows Powershell Cmdlet based on MVC pattern. Allow to look into the details of what's going on inside Powershell command pipeline
+
+<img src="https://raw.githubusercontent.com/TurboBasic/dotfiles.windows/master/hexagram.png" alt="Windows Dotfiles" style="width: 100px; height: auto;" />
---
+++ test-pipeline.ps1
@@ -0,0 +1,168 @@
+﻿# Helps to get detailed structure of pipelined data inside cmdlet
+# Fully implements MVC
+
+Function model__New-Content ($__computerName, $__PSBoundParameters) {
+  @{
+    content = @{
+        computerType = $( try   { $__computerName.GetType() } 
+                          catch { $null }
+                        );
+        computerName = $($__computerName | Out-String).trimEnd("`n");
+        callParameters = $(
+            $__PSBoundParameters | Format-Table -AutoSize | Out-String
+        )
+    }
+  }
+}
+
+
+Function view__Get-Style ( $level ) {
+  $Styles = @{
+    levelBegin = @{
+      margin = " " * 0
+      color =  "Gray"
+    }
+    levelProcess = @{
+      margin = " " * 4
+      color =  "White"
+    }
+    levelProcessEach = @{
+      margin = " " * 12
+      color =  "Yellow"
+    }
+    levelEnd = @{
+      margin = " " * 0
+      color =  "Green"
+    }
+  }
+
+  @{ 
+    style = $Styles[$level]
+  }
+}
+
+
+Function view__Get-ContentTemplate ( $level ) {
+
+  $Template = @'
+{0}{{header}}
+{0}   {{item}} is a {1} with value {2}   
+{0}   $PSBoundParameters is {3}
+'@
+
+  $Placeholders = @{
+    header = @{
+      levelBegin =       "BEGIN Block -"
+      levelProcess =     "PROCESS Block -"
+      levelProcessEach = "PROCESS Block, FOREACH LOOP -"
+      levelEnd =         "END Block - "
+    }
+    
+    item = @{
+      levelBegin =       '$ComputerName'
+      levelProcess =     '$ComputerName'
+      levelProcessEach = 'processed item'
+      levelEnd =         '$ComputerName'
+    }
+  }
+
+
+  $Template | ForEach-Object {
+    [Regex]::Replace($_, '{{(\p{L}+)}}', {
+      param($Match)
+      return $Placeholders[$Match.Groups[1].Value][$level]
+    })
+  } | ForEach-Object { @{ template = $_ } }
+
+}
+
+
+Function view__Display-Text($style, $template, $content) {
+  $callParametersStyled = $content.callParameters -split "`n" |
+    ForEach-Object { $style.margin + (" "*6) + $_ } |
+    Out-String
+
+  $text = $template -f $style.margin, $content.computerType, $content.computerName, $callParametersStyled
+
+  Write-Host $text -foregroundColor $style.color
+}
+
+
+Function controller__Print-Message($level, $content) {
+
+  $viewParams  = @{}
+  $viewParams += $content
+  $viewParams += view__Get-ContentTemplate $level
+  $viewParams += view__Get-Style $level 
+
+  view__Display-Text @viewParams
+
+}
+
+
+Function Test-Pipeline {
+  #region Parameters
+    [cmdletbinding(SupportsShouldProcess=$true, ConfirmImpact="Medium")]            
+    param(            
+        [parameter( Mandatory = $false,            
+                    ValueFromPipeline = $True,            
+                    ValueFromPipelineByPropertyName = $True)]            
+        [string[]]$ComputerName = "$env:computername",            
+            
+        [switch]$Force            
+    ) 
+  #endregion
+  
+            
+  Begin {            
+    $RejectAll = $false            
+    $ConfirmAll = $false
+    
+    $ComputerName | Out-String -Stream | Write-Verbose
+
+    $model__initialContent = model__New-Content $ComputerName $PSBoundParameters
+    controller__Print-Message -Level "levelBegin" -Content $model__initialContent
+  }
+
+            
+  Process {
+    #region ConfirmationQuestions
+      Function ShouldIExecuteAction ($target) {
+        $query = "Are you REALLY sure you want to process ${target}?"
+        $caption = "Processing ${target}"
+
+        return ($Force -Or $PSCmdlet.ShouldContinue($query, $caption, [ref]$ConfirmAll, [ref]$RejectAll))
+      }
+
+      Function ShouldIProcessItem ($target) {
+        $verboseDescription = "Processed the computer ${target}"
+        $verboseWarningQuestion = "Process the computer ${target}?" 
+        $caption = "Processing computer"
+        return $PSCmdlet.ShouldProcess( $verboseDescription, $verboseWarningQuestion, $caption )
+      }
+    #endregion
+
+    $ComputerName | Out-String -Stream | Write-Verbose
+
+    $model__loopContent = model__New-Content $ComputerName $PSBoundParameters
+    controller__Print-Message -Level "levelProcess" -Content $model__loopContent    
+                
+    foreach($Computer in $ComputerName) {
+      $model__singleItemContent = model__New-Content $Computer $PSBoundParameters
+
+      if( ShouldIProcessItem $model__singleItemContent.content.computerName ) {
+        if( ShouldIExecuteAction($model__singleItemContent.content.computerName) ) {  
+          controller__Print-Message -Level "levelProcessEach" -Content $model__singleItemContent
+        }            
+      }            
+    }            
+  }
+
+            
+  End {
+    $model__endContent = model__New-Content $ComputerName $PSBoundParameters
+    controller__Print-Message -Level "levelEnd" $model__endContent         
+  }            
+}
+
+New-Alias p Test-Pipeline -Force
